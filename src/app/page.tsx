5af59c74ae7a9506dafe6a452adb2461a7072474
JuanHernandezC2024 <jh.consuegra75@gmail.com>
2025-08-07 20:37:17 -0500

    correcciones para mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
     </section>
     <section id="About" className="p-10 flex flex-col items-center"> 
     <Image src={"https://res.cloudinary.com/dvyrs30i1/image/upload/v1754612384/noBusques_iasvuq.png"} className="mt-10 min-w-[180px] min-h-[60px] hover:scale-105 grayscale-100 hover:grayscale-0 transition-all duration-200 cursor-pointer" alt="Introducción" width={500} height={300} />
-    <p className="font-bold text-center">I am Juan, a frontend developer focused on creating scalable, responsive, and visually appealing interfaces. I work with Next.js, React, and TailwindCSS, optimizing every project for efficient deployment and a flawless user experience. <span>If you want to get in touch, <Link className=" hover:underline text-blue-950" href={"mailto:jh.consuegra75@gmail.com"}>here is my email</Link></span>
+    <p className="font-bold text-black text-center">I am Juan, a frontend developer focused on creating scalable, responsive, and visually appealing interfaces. I work with Next.js, React, and TailwindCSS, optimizing every project for efficient deployment and a flawless user experience. <span>If you want to get in touch, <Link className=" hover:underline text-blue-950" href={"mailto:jh.consuegra75@gmail.com"}>here is my email</Link></span>
     </p>
     <Image src={"https://res.cloudinary.com/dvyrs30i1/image/upload/v1754612383/imagen_perfil_cv_cfpia3.jpg"} className="mt-10 rounded-full border-2 border-black hover:scale-105 transition-transform duration-200 cursor-pointer shadow-2xl" alt="Introducción" width={300} height={500}  />
     <div className="flex flex-wrap items-baseline-last justify-center gap-5 mt-10 mb-10">
@@ -47,8 +47,8 @@ export default function Home() {
       link="https://electric-bikes-e-commerce.vercel.app/"
     />
     <ProyectCard
-      title="🧭 Import Management System"
-      description="🌐 Web Application Overview
+      title="Import Management System"
+      description="Web Application Overview
 This web application is built with Next.js on the frontend and NestJS on the backend, specifically designed to optimize the management of business logistics and import processes.
 "
       imageUrl="https://res.cloudinary.com/dvyrs30i1/image/upload/v1754612384/TradeTrack_nrlgi0.png"
--- src/components/ImgText.tsx
+++ src/components/ImgText.tsx
@@ -7,7 +7,7 @@ export const Imgtext = ({ imageUrl, text, link }: { imageUrl: string; text: stri
             <Link className="hover:scale-105 transition-transform duration-200" href={link}>
             <Image src={imageUrl} alt={text} width={100} height={100} className="rounded-full grayscale-100 hover:grayscale-0 transition-all duration-200" />
             </Link>
-            <p className="font-bold">{text}</p>
+            <p className="font-bold text-black">{text}</p>
         </div>
     )
 }
